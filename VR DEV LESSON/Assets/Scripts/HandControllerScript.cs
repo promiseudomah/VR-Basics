005288d369bafde7b1ec2250e2ef93d3446b9429
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-09-30 16:53:16 +0100

    VR Locomotion Fixed

---
+++ VR DEV LESSON/Assets/Scripts/HandControllerScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class HandControllerScript : MonoBehaviour
+{
+    [SerializeField] InputActionReference gripInputAction;
+    [SerializeField] InputActionReference triggerInputAction;
+
+    Animator handAnmator;
+
+    private void Awake()
+    {
+        handAnmator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        gripInputAction.action.performed += GripPressed;
+        triggerInputAction.action.performed += TriggerPressed;
+    }
+
+    private void TriggerPressed(InputAction.CallbackContext obj)
+    {
+       handAnmator.SetFloat("Trigger", obj.ReadValue<float>());
+    }
+
+    private void GripPressed(InputAction.CallbackContext obj)
+    {
+        handAnmator.SetFloat("Grip", obj.ReadValue<float>());
+    }
+
+    private void OnDisable()
+    {
+        gripInputAction.action.performed -= GripPressed;
+        triggerInputAction.action.performed -= TriggerPressed;
+    }
+}
